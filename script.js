dd2248d253f94ae1641336f25ee87d1242bd9286
Sultan <Soltan6@hotmail.fr>
2019-05-31 20:22:06 +0300

    display action

--- script.js
+++ script.js
@@ -14,25 +14,40 @@ function game(e){
       let computerGuess = ['rock','paper','scissors'];
       return computerGuess[Math.floor(Math.random() * 3)];
     }
-    
     console.log('COM: ' + computerSelection)
     console.log(e.target)
     playRound(computerSelection,e);
     actionDisplay(computerSelection,e);
 }
-
+//match up
 function playRound(computerSelection,e) {
   if (computerSelection === e.target.id) {
     console.log('Draw')
-    
  }
 }
-//switch cases to display action
+
+//switch to display action
 function actionDisplay(computerSelection,e) {
   switch (e.target.id) {
-    case '#rock':
+    case 'rock':
       clickedRock.style.display = 'block';
-      console.log('hahah');
+      break;
+      case 'paper':
+      clickedPaper.style.display = 'block';
+      break;
+      case 'scissors':
+      clickedScissors.style.display = 'block';
+      break;
+  }
+  switch (computerSelection) {
+    case 'rock':
+      compGuess_R.style.display = 'block';
+      break;
+      case 'paper':
+      compGuess_P.style.display = 'block';
+      break;
+      case 'scissors':
+      compGuess_S.style.display = 'block';
       break;
   }
 }
